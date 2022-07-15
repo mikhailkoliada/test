73f65ad9028f2a5c799b2ffcdd099f3d5836f01f
Mikhail Koliada <88318005+mikhailkoliada@users.noreply.github.com>
2022-07-15 22:33:21 +0200

    Update swkiller.sh

--- swkiller.sh
+++ swkiller.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo kill -9 $(pgrep -f "/usr/sbin/softwareupdate --all") || true; rm /tmp/swupd.log
+sudo kill -3 $(pgrep -f "/usr/sbin/softwareupdate --all") || true; rm /tmp/swupd.log
